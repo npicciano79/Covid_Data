092194b26aa43e41b5c60b1df431d0d3d8b0e0a6
npicciano79 <npicciano79@gmail.com>
2023-01-31 21:53:17 -0500

    main commit

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-document.addEventListener('DOMContentLoaded',getData());
 
 const countryPath=document.querySelectorAll('svg path');
 
@@ -6,8 +5,8 @@ const countryPath=document.querySelectorAll('svg path');
 
 
 //async function calls api and returns covid data
-async function getData(countryName){
-    //console.log(countryName);
+async function getData(){
+    console.log('hit');
     try{
         const response=await fetch(
         'https://covid-19-statistics.p.rapidapi.com/reports',
@@ -20,7 +19,9 @@ async function getData(countryName){
         }
     });
         const RawData=await response.json();
-        console.log(RawData.data);
+        //console.log(RawData.data);
+
+        return RawData;
 
     
     }catch (error){
@@ -32,7 +33,12 @@ async function getData(countryName){
 
 
 function getCOVIDData(countryName){
+    COVIDdata=localStorage.getItem('COVIDdata');
+    console.log(COVIDdata);
     console.log(countryName);
+   
+    
+   
 
 }
 
@@ -45,8 +51,16 @@ function getCOVIDData(countryName){
 countryPath.forEach(path=>{
     path.addEventListener('mouseover',()=>{
         const countryName=path.className.baseVal;
-        
+
         getCOVIDData(countryName);
+        
 
     })
 })
+
+
+
+//call getData on pageload
+const tempData=getData()
+localStorage.setItem('COVIDdata',tempData);
+
