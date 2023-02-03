4497b1f90259201b4d545a11e91fddab71b8d5cd
npicciano79 <npicciano79@gmail.com>
2023-02-03 06:20:33 -0500

    commit

--- index.js
+++ index.js
@@ -1,66 +1,78 @@
 
+
 const countryPath=document.querySelectorAll('svg path');
 
+window.onload=function(){
+    main();
+    //console.log(COVIDdata);
+
+}
 
 
+function main(){
+    //async function calls api and returns covid data
+    async function getData(){
+        try{
+            const response=await fetch(
+            'https://covid-19-statistics.p.rapidapi.com/reports',
+            {
+                method:'GET',
+                headers:{
+                    'X-RapidAPI-Host': 'covid-19-statistics.p.rapidapi.com',  
+                    'X-RapidAPI-Key':'a18aa547acmsh0e4bcca5a5d0cd6p143347jsn579efc41eebf'
 
-//async function calls api and returns covid data
-async function getData(){
-    console.log('hit');
-    try{
-        const response=await fetch(
-        'https://covid-19-statistics.p.rapidapi.com/reports',
-        {
-            method:'GET',
-            headers:{
-                'X-RapidAPI-Host': 'covid-19-statistics.p.rapidapi.com',  
-                'X-RapidAPI-Key':'a18aa547acmsh0e4bcca5a5d0cd6p143347jsn579efc41eebf'
+            }
+        });
+            const RawData=await response.json();
+            //console.log(RawData.data);
 
+            return RawData;
+
+        
+        }catch (error){
+            console.log(`ERROR: ${error}`);
         }
-    });
-        const RawData=await response.json();
-        //console.log(RawData.data);
 
-        return RawData;
 
-    
-    }catch (error){
-        console.log(`ERROR: ${error}`);
     }
 
 
-}
-
-
-function getCOVIDData(countryName){
-    COVIDdata=localStorage.getItem('COVIDdata');
-    console.log(COVIDdata);
-    console.log(countryName);
-   
+    function getCOVIDData(countryName){
+        //COVIDdata=localStorage.getItem('COVIDdata');
+        //console.log(COVIDdata);
+        console.log(countryName);
     
-   
+        
+    
+
+    }
+
+
+
+
+    //event listener, on mouse over gets country name
+    countryPath.forEach(path=>{
+        path.addEventListener('mouseover',()=>{
+            const countryName=path.className.baseVal;
+
+            getCOVIDData(countryName);
+            
+
+        })
+    })
+
+
+
+    async function callAsync(){
+        const x = await getData();
+        return x; 
+    }
+
+
+   const COVIDdata=callAsync();
+   console.log(COVIDdata);
 
 }
 
 
-
-
-
-
-//event listener, on mouse over gets country name
-countryPath.forEach(path=>{
-    path.addEventListener('mouseover',()=>{
-        const countryName=path.className.baseVal;
-
-        getCOVIDData(countryName);
-        
-
-    })
-})
-
-
-
-//call getData on pageload
-const tempData=getData()
-localStorage.setItem('COVIDdata',tempData);
 
